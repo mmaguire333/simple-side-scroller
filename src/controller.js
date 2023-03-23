cadc44f201d8c04b345cc52e4806c63f6880ee91
Matt <mmaguire333@gmail.com>
2023-03-23 17:21:02 -0600

    Implement controller class with up, left, and right variables representing keyboard button inputs, and implement methods that can set isDown value for up, left, and right variables when passed a keyboard event

--- src/controller.js
+++ src/controller.js
@@ -1,3 +1,43 @@
  class Controller {
-    
+    constructor() {
+        this.left = new Controller.ButtonInput();
+        this.right = new Controller.ButtonInput();
+        this.up = new Controller.ButtonInput();
+    }
+
+    // function that is passed an event, if it's a keydown event the is down property on the corresponding key (up, left, right) will be set true
+    // if its not a keydown event (then it will be key up event) the iskeydown property on corresponding key will be set false
+    keyDownOrUp(event) {
+        let isKeyDown;
+        if(event.type === 'keydown') {
+            isKeyDown = true;
+        } else {
+            isKeyDown = false;
+        }
+
+        if(event.key === 'ArrowLeft') {
+            this.left.getInput(isKeyDown);
+            console.log("left is down: " + isKeyDown);
+        } else if(event.key === 'ArrowUp') {
+            this.up.getInput(isKeyDown);
+            console.log("up is down: " + isKeyDown);
+        } else if(event.key === 'ArrowRight') {
+            this.right.getInput(isKeyDown);
+            console.log("right is down: " + isKeyDown);
+        }
+    }
+ }
+
+
+ Controller.ButtonInput = class {
+    constructor() {
+        this.isDown = false;
+    }
+
+    // method to change isDown property of ButtonInput object (or leave it the same if input matches the current value)
+    getInput(isDown) {
+        if(this.isDown != isDown) {
+            this.isDown = isDown;
+        }
+    }
  }
