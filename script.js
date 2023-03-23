82740e212a3027e35f58382a619baeee4e70db68
Matt <mmaguire333@gmail.com>
2023-03-22 19:51:27 -0600

    Add modules for game logic, controller logic, display logic and engine logic and move all jscode into src folder

--- script.js
+++
@@ -1,5 +0,0 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-
-canvas.width = 400;
-canvas.height = 1000;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = 1200;
+canvas.height = 400;
+
