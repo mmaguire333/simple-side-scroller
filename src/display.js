a7c6953c1f3a565f24f90d3c2a67dd59ce58f754
Matt <mmaguire333@gmail.com>
2023-03-27 17:26:48 -0600

    Implement display class with a function that draws the background of the buffer context, another function that draws a rectangle onto the buffer context, and another function that draws to buffer context onto the on screen canvas

--- src/display.js
+++ src/display.js
@@ -1,3 +1,20 @@
 class Display {
-    
+    constructor(canvas) {
+        this.buffer = document.createElement('canvas').getContext('2d');
+        this.context = canvas.getContext('2d');
+    }
+
+    drawRectangle(x, y, width, height, color) {
+        this.buffer.fillStyle = color;
+        this.buffer.fillRect(x, y, width, height);
+    }
+
+    fill(color) {
+        this.buffer.fillStyle = color;
+        this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
+    }
+
+    render() {
+        this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height);
+    }
 }
