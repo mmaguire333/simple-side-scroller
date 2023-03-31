cf29eef432d22ce1e705c3f07e67cfc26d982ca9
Matt <mmaguire333@gmail.com>
2023-03-31 16:16:58 -0600

    load background image, tileset image, load tilemap into game.world.tilemap array, and update rnder and update functions to draw background image and platforms to the screen and implement scrolling behavior

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 //import Game from './game'
 
-
 // function to resize on screen canvas
 const resize = function(event) {
     display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, game.world.height / game.world.width);
@@ -9,19 +8,41 @@ const resize = function(event) {
 
 // function to render game world on screen
 const render = function() {
-    display.fill(game.world.backgroundColor);
+    // clear content from previous frame
+    display.buffer.clearRect(0, 0, display.buffer.canvas.width, display.buffer.canvas.height);
+    display.context.clearRect(0, 0, display.context.canvas.width, display.context.canvas.height);
+
+    // draw content for next frame
+    display.drawBackground(game.world.backgroundImage, game.world.backgroundOffset);
+    display.drawMap(game.world.tilemap, 500, 16, game.world.backgroundOffset);
     display.drawRectangle(game.world.player.x, game.world.player.y, game.world.player.width, game.world.player.height, 'red');
-    display.render(); 
+    display.render();
+
+    // update offset for scrolling effect
+    game.world.backgroundOffset -= game.world.scrollSpeed;
+    if(game.world.backgroundOffset > 0) {
+        game.world.backgroundOffset = 0;
+    }
+
+    if(game.world.backgroundOffset < -7000) {
+        game.world.backgroundOffset = -7000;
+    }
 }
 
 // function to update state of game world based on user input
 const update = function() {
     if(controller.left.isDown) {
         game.world.player.moveLeft();
+        game.world.scrollSpeed = -7;
     }
 
     if(controller.right.isDown) {
         game.world.player.moveRight();
+        game.world.scrollSpeed = 7;
+    }
+
+    if(!controller.left.isDown && !controller.right.isDown) {
+        game.world.scrollSpeed = 0
     }
 
     if(controller.up.isDown) {
@@ -42,6 +63,8 @@ const engine = new Engine(1000/30, update, render);
 display.buffer.canvas.height = game.world.height;
 display.buffer.canvas.width = game.world.width;
 
+// event listeners
+
 // sets isDown property for controller up, right, or left variable to true when up, right, or left key is down
 window.addEventListener('keydown', (event) => {
     controller.keyDownOrUp(event);
@@ -55,6 +78,18 @@ window.addEventListener('resize', (event) => {
     resize(event);
 });
 
-resize();
+// loads tilemap into game.world tilemap variable and tileset image into display.tileset src
+game.world.loadTilemap('../tilemaps/platform-sidescroller.json');
 
-engine.start();
+// sets background image src once image is loaded
+game.world.backgroundImage.src = 'platform-sidescroller-background.png';
+
+// sets the tileset image src
+display.tiles.image.src = '../tilesets/SET1_Mainlev_build.png';
+
+
+// FIXME -- find a better way to do this, right now it is just a cheap way to make sure all the images and tilemap are loaded before starting engine
+setTimeout(() => {
+    resize();
+    engine.start();
+}, 500);
