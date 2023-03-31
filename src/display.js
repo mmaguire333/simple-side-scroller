398859d98eb94e6837f2f2006b40e46b4cf161e9
Matt <mmaguire333@gmail.com>
2023-03-31 16:03:46 -0600

    Add a tileset class and a drawmap function to draw a tilemap to the screen

--- src/display.js
+++ src/display.js
@@ -2,6 +2,27 @@ class Display {
     constructor(canvas) {
         this.buffer = document.createElement('canvas').getContext('2d');
         this.context = canvas.getContext('2d');
+        this.tiles = new Display.Tileset(16, 34);
+    }
+
+    drawMap(map, numCols, mapTileSize, offset) {
+        for(let i = 0; i < map.length; i++) {
+            // the tileset were using to draw the platforms is the third tileset used when creating the background in tiled
+            // checking the platform-sidescroller.json shows this tileset has a firstgid of 4740
+            // therefore we substract 4740 to get the true index of the correct tile in the tileset
+            let tileIndex = map[i] - 4740;
+
+            let sx = (tileIndex % this.tiles.columns) * this.tiles.tileSize;
+            let sy = Math.floor(tileIndex / this.tiles.columns) * this.tiles.tileSize;
+            let dx = (i % numCols) * mapTileSize;
+            let dy = Math.floor(i / numCols) * mapTileSize;
+
+            this.buffer.drawImage(this.tiles.image, sx, sy, this.tiles.tileSize, this.tiles.tileSize, dx + offset, dy, mapTileSize, mapTileSize);
+        }
+    }
+
+    drawBackground(image, offset) {
+        this.buffer.drawImage(image, offset, 0);
     }
 
     drawRectangle(x, y, width, height, color) {
@@ -26,5 +47,13 @@ class Display {
             this.context.canvas.height = height;
             this.context.canvas.width = height / heightWidthRatio;
         }
+    }
+}
+
+Display.Tileset = class {
+    constructor(tileSize, columns) {
+        this.image = new Image();
+        this.tileSize = tileSize;
+        this.columns = columns;
     }
 }
