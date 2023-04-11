bdc2652622a4df97c684ed74c30b998f0c51ba2b
Matt <mmaguire333@gmail.com>
2023-04-11 16:18:55 -0600

    Change img paths

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import Engine from './engine';
 
 // function to resize on screen canvas
 const resize = function() {
-    console.log(document.body.clientWidth)
     let margin = 0;
     if(document.body.clientWidth > 1650) {
         margin = 250
@@ -114,27 +113,27 @@ window.addEventListener('resize', () => {
 });
 
 // sets background image src once image is loaded
-game.world.backgroundImage.src = './platform-sidescroller-background.png';
+game.world.backgroundImage.src = 'platform-sidescroller-background.png';
 
 // sets the tileset image src
-display.tiles.image.src = './tilesets/SET1_Mainlev_build.png';
+display.tiles.image.src = 'tilesets/SET1_Mainlev_build.png';
 
 // sets player spritesheet images
-game.world.player.idleSpriteSheet.src = './character_spritesheets/_Idle.png';
-game.world.player.idleBackwardsSpriteSheet.src = './character_spritesheets/_Idle_mirrored.png';
-game.world.player.runSpriteSheet.src = './character_spritesheets/_Run.png';
-game.world.player.runBackwardsSpriteSheet.src = './character_spritesheets/_Run_mirrored.png'; 
-game.world.player.jumpSpriteSheet.src = './character_spritesheets/_Jump.png';
-game.world.player.jumpBackwardsSpriteSheet.src = './character_spritesheets/_Jump_mirrored.png';
-game.world.player.jumpFallTransitionSpriteSheet.src = './character_spritesheets/_JumpFallInbetween.png';
-game.world.player.jumpFallTransitionBackwardsSpriteSheet.src = './character_spritesheets/_JumpFallInbetween_mirrored.png';
+game.world.player.idleSpriteSheet.src = 'character_spritesheets/_Idle.png';
+game.world.player.idleBackwardsSpriteSheet.src = 'character_spritesheets/_Idle_mirrored.png';
+game.world.player.runSpriteSheet.src = 'character_spritesheets/_Run.png';
+game.world.player.runBackwardsSpriteSheet.src = 'character_spritesheets/_Run_mirrored.png'; 
+game.world.player.jumpSpriteSheet.src = 'character_spritesheets/_Jump.png';
+game.world.player.jumpBackwardsSpriteSheet.src = 'character_spritesheets/_Jump_mirrored.png';
+game.world.player.jumpFallTransitionSpriteSheet.src = 'character_spritesheets/_JumpFallInbetween.png';
+game.world.player.jumpFallTransitionBackwardsSpriteSheet.src = 'character_spritesheets/_JumpFallInbetween_mirrored.png';
 
 // loads data about the sprite sheets into the players spriteAnimations array
 game.world.player.populateSpriteAnimations();
 
 // loads tilemap into game.world tilemap variable and tileset image into display.tileset src
 // this is done asyncronously (loadMaps is async function), so we resolve the promise before starting game engine
-game.world.loadMaps('./tilemaps/platform-sidescroller.json').then(() => {
+game.world.loadMaps('tilemaps/platform-sidescroller.json').then(() => {
     resize();
     engine.start();
 });
